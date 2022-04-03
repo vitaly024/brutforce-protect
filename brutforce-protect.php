d99a62f36e63034f11d0b76321ae1863fbcdb09e
Vitaly <vitaly024@gmail.com>
2022-04-03 12:53:34 +0300

    1.1.7 - Alt_link

--- brutforce-protect.php
+++ brutforce-protect.php
@@ -2,12 +2,13 @@
 /*
 Plugin Name: Brute force protect (BFP)
 Description: Обеспечивает защиту от брутфорса по логину и паролю.
-Version: 1.1.6
+Version: 1.1.7
 Author: Виталий Головков
 License: GPLv2 or later
 */
 
 /* Updates:
+1.1.7 - ошибка при инсталяции - исправлен $alt_link	03.04.2022
 В версии 1.1.6 уточнено сообщение о бруте "Пароль не верен. Число возможных попыток до блокировки: 1 " 
 В версии 1.1.5 исправлена ошибка: невозможно войти если с этого компа уже был вход. (добавлен принудительный выход перед проверками + проверка на пустой логин передвинута в самое начало )
 В версии 1.1.4 исправлена ошибка с входом нового пользователя (не зареганного в таблице брутфорс-атак)
@@ -24,10 +25,10 @@ register_activation_hook(__FILE__ , 'bfp_activate' );
 //register_deactivation_hook(__FILE__ , 'bfp_deactivate' );
 register_uninstall_hook(__FILE__ , 'bfp_uninstall');
 
-if (!isset($alt_link) ){ //если не установлено в файле wp-config
-	$alt_link = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME); 
-	$alt_link->query('set names utf8');
-	$alt_link->query("set lc_time_names='ru_RU'");
+if (!isset($alt_link2) ){ //если не установлено в файле wp-config
+	$alt_link2 = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME); 
+	$alt_link2->query('set names utf8');
+	$alt_link2->query("set lc_time_names='ru_RU'");
 }
 
 //после логина переброс на главную страницу
@@ -55,16 +56,16 @@ function bfp_check_brutforse( $user, $username, $password ){
 
 //Функция выполняемая при активации плагина
 function bfp_activate() {	
-	global $alt_link;
-	istall_wp_login_hash_table($alt_link);	//создать таблицу
+	$alt_link3 = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME); 
+	istall_wp_login_hash_table($alt_link3);	//создать таблицу
 	bfp_set_variables();					//инициализировать переменные	
 }
 
 
 //Функция выполняемая при удалении плагина
 function bfp_uninstall() {
-	global $alt_link;
-	drop_wp_login_hash_table($alt_link); 	//грохнуть таблицу
+	$alt_link3 = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME); 
+	drop_wp_login_hash_table($alt_link3); 	//грохнуть таблицу
 	bfp_drop_variables();					//грохнуть переменные
 }
 
